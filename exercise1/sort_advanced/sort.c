c433bca0e94892e2c983f68d53dc2d1258b8955e
Samuel Chatfield <sxc678@cs.bham.ac.uk>
2019-10-17 23:21:10 +0100

    Sort_advanced working.

--- exercise1/sort_advanced/sort.c
+++ exercise1/sort_advanced/sort.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_STRINGS 20
-
 char *getline()
 {
 	size_t linesize = 0;
@@ -12,14 +10,15 @@ char *getline()
 
 	do {
 		linesize += 20;
-		// printf("linesize: %d\n", linesize);
-		line = realloc(line, linesize);
-		// if (fgets(line+last, linesize, stdin) == NULL)
-		// 	return line;
-		fgets(line+len, 20, stdin);
+		line = (char *) realloc(line, linesize * sizeof(char));
+		if (!line) exit(1);
+
+		if (fgets(line+len, 20, stdin) == NULL) {
+			free(line);
+			return NULL;
+		}
+
 		len = strlen(line);
-		// printf("line: '%s'\n", line);
-		// printf("len: %d\n", strlen(line));
 	} while (!feof(stdin) && line[len-1] != '\n');
 
 	return line;
@@ -34,46 +33,53 @@ int cmpstr(const void *a, const void *b)
 
 int main(int argc, char **argv)
 {
-	char *strings[MAX_STRINGS];
+	char **strings = NULL;
 
-	int len, linecount;
+	int len;
 	char *sp, *linebuf;
+	size_t linesmax = 0;
+	size_t linecount = 0;
 
 	// Read in the strings from stdin, accounting for case of no newline at the end
-	linecount = 0;
 	while ((linebuf = getline()) != NULL) {
 		len = strlen(linebuf);
 		if (len == 0) {
 			free(linebuf);
 			break;
 		}
-		printf("linecount: %d, len: %d, line: '%s'\n", linecount, len, linebuf);
 		if (linebuf[len-1] == '\n') {
 			// If the line has a newline at the end, replace it with null byte
 			linebuf[len-1] = '\0';
 		} else {
 			// If there is no newline at the end, append a null byte and increment line length
-			linebuf[len] = '\0';
-			len++;
+			linebuf[len++] = '\0';
 		}
-		if (linecount >= MAX_STRINGS || (sp = malloc(len)) == NULL) {
-			fprintf(stderr, "Error: number of lines exceeded maximum of %d", MAX_STRINGS);
-			return 1;
-		} else {
-			strncpy(sp, linebuf, len);
-			strings[linecount++] = sp;
+
+		sp = (char *) malloc(len * sizeof(char));
+		if (!sp) return 1;
+
+		if (linecount == linesmax) {
+			linesmax += 20;
+			strings = (char **) realloc(strings, linesmax * sizeof(char *));
+			if (!strings) return 1;
 		}
+
+		strncpy(sp, linebuf, len);
+		strings[linecount++] = sp;
+
 		free(linebuf);
 	}
 
 	// Sort the strings using Q-Sort library function
-	qsort(strings, linecount, sizeof(const char *), cmpstr);
+	qsort(strings, linecount, sizeof(char *), cmpstr);
 
 	// Print the (now sorted) strings
 	for (int i = 0; i < linecount; i++) {
 		printf("%s\n", strings[i]);
 		free(strings[i]);
 	}
+
+	free(strings);
 
 	return 0;
 }
--- exercise1/sort_advanced/test.sh
+++ exercise1/sort_advanced/test.sh
@@ -7,6 +7,8 @@ diff /tmp/in1_sorted.txt in1_sorted.txt
 diff /tmp/in2_sorted.txt in2_sorted.txt
 ./sort < in3.txt > /tmp/in3_sorted.txt
 diff /tmp/in3_sorted.txt in3_sorted.txt
+./sort < in4.txt > /tmp/in4_sorted.txt
+diff /tmp/in4_sorted.txt in4_sorted.txt
 ./sort < faust.txt > /tmp/faust_sorted.txt
 diff /tmp/faust_sorted.txt faust_sorted.txt
 echo OK
